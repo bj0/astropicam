08bbcc840ea7182db0630db9b98924f393516055
Brian Parma <execrable@gmail.com>
2018-03-28 17:27:55 -0700

    update plot display

--- picam/plotscreen.py
+++ picam/plotscreen.py
@@ -6,15 +6,15 @@ from kivy.utils import get_color_from_hex as rgb
 class PlotScreen(Screen):
     def init(self):
         # use kivy garden's graph widget
-        graph = Graph(xlabel='x', ylabel='F', x_ticks_minor=5, x_ticks_major=10,
+        graph = Graph(xlabel='t', ylabel='HFR', x_ticks_minor=5, x_ticks_major=10,
                       y_ticks_minor=1, y_ticks_major=5, y_grid_label=True,
                       x_grid_label=False, padding=0, y_grid=False, x_grid=False,
                       xmin=0, ymin=0, xmax=100, ymax=30,
                       _with_stencilbuffer=False,  # or it does not work in ScreenManager
-                      label_options={'color': rgb('000000')})
+                      label_options={'color': rgb('808080')})
         y = (float(i) for i in range(50))
         x = (float(i) for i in range(50))
-        plot = MeshLinePlot(color=rgb('1100ff'))
+        plot = MeshLinePlot(color=rgb('1100aa'))
         pts = list(zip(x, y))
         plot.points = pts
         graph.add_plot(plot)
