e870c09af33af2b0f0968194359c7f99a5dc5f49
Brian Parma <execrable@gmail.com>
2018-03-28 17:34:57 -0700

    add some config values

--- picam/config.py
+++ picam/config.py
@@ -1,2 +1,6 @@
 # flag to signal that rpi could not be imported, use 'fake' commands
 FAKE = False
+FAKE_FRAMES_DIR = "3"  # Path to fake frames
+
+MEASURE_RATE = 5  # measure ever N frames
+FRAME_RATE = 10
