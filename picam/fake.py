8d95b77621aca327e53dd1e7de4abff0cef73fb5
Brian Parma <execrable@gmail.com>
2018-03-31 13:52:22 -0700

    zooming on fake switches preview frames
    cleanup

--- picam/fake.py
+++ picam/fake.py
@@ -7,12 +7,23 @@ class FakeSource(object):
     """
     a fake source of video using previously captured frames
     """
-    frame_path = Path(cfg.FAKE_FRAMES_DIR)
+
+    def __init__(self):
+        self._changed = False
+        self._index = 0
+        self.frame_path = [Path(x) for x in cfg.FAKE_FRAMES_DIR]
 
     def __iter__(self):
         while True:
-            for frame in self.frame_path.iterdir():
+            self._changed = False
+            for frame in self.frame_path[self._index].iterdir():
+                if self._changed:
+                    break
                 with open(frame, 'rb') as f:
                     buf = f.read()
                 yield buf
 
+    def zoom(self):
+        self._index = (self._index + 1) % 3
+        print('now {}'.format(self.frame_path[self._index]))
+        self._changed = True
